a9f714b6ac514e52ff3b7600ef6dd5d285a6b06a
ravikt <ravikt@tamu.edu>
2021-10-29 00:04:27 -0500

    updated viz

--- viz.py
+++ viz.py
@@ -2,11 +2,20 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-dfile = "results.npz"
-images = np.load(dfile)["heatmap"]
-print(images.shape)
+dfile = np.load("results.npz")
+print(dfile.keys())
+ghmap = dfile["heatmap"]
+#img   = dfile["images"]
+
+#print(images.shape)
 
 
-plt.figure()
-plt.imshow(images[5000])
-plt.savefig('gaze_5000.png')
+idx = [0, 44, 1000, 5000]
+#plt.figure()
+
+for i in idx:
+   #plt.imshow(img[i])
+   #plt.savefig(f'img_{i}.png')
+
+   plt.imshow(ghmap[i])
+   plt.imsave(f'gh_{i}.png', ghmap[i])
