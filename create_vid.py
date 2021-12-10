797752c4baf7c39a3d57c234e8723b6af979ec61
ravikt <ravikt@tamu.edu>
2021-12-10 14:10:38 -0600

    added overlay function

--- create_vid.py
+++ create_vid.py
@@ -10,8 +10,9 @@ import os
 import re
 import datetime
 
-def overlay():
-    pass
+def overlay(img, ghmap):
+    output = cv2.addWeighted(ghmap, 0.4, img, 0.6, 0.0)
+    return output
 
 def reshape_image(image):
     """Warp frames to 84x84 as done in the Nature paper and later work."""
@@ -23,17 +24,24 @@ def reshape_image(image):
     return frame
     #return frame / 255.0 
 
-def video(img_dir):
-    files = os.listdir(img_dir)
-    files.sort(key=lambda f: int(re.sub('\D', '', f)))
+def video(img_dir, ghmap_dir):
+    img_files = os.listdir(img_dir)
+    img_files.sort(key=lambda f: int(re.sub('\D', '', f)))
+
+    ghmap_files = os.listdir(ghmap_dir) 
+    ghmap_files.sort(key=lambda g: int(re.sub('\D', '', g)))
+
     fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
     video = cv2.VideoWriter('video.avi', fourcc, 1, (224, 224))
 
-    for img_path in files:
+    for (img_path,ghmap_path) in zip(img_files, ghmap_files):
        image = cv2.imread(os.path.join(img_dir, img_path))
        image = np.uint8(reshape_image(image))
        print(image.shape)
-       video.write(image)
+
+       ghmap = cv2.imread(os.path.join(ghmap_dir, ghmap_path))
+       output = overlay(image, ghmap)
+       video.write(output)
 
     cv2.destroyAllWindows()
     video.release()
@@ -41,9 +49,10 @@ def video(img_dir):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Script for writing videos")
-    parser.add_argument("-i", "--inpath", type=str, help="path to the folder")   
+    parser.add_argument("-i", "--inpath", type=str, help="path to the image folder")   
+    parser.add_argument("-g", "--ghpath", type=str, help="path to the gaze golder") 
     args = parser.parse_args()
     # Path for model and the data
     input_path = args.inpath
-    
-    video(input_path)
+    gaze_path  = args.ghpath
+    video(input_path, gaze_path)
