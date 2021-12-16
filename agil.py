969504bd0ed955824093b7a82e0d46720a9649c5
ravikt <ravikt@tamu.edu>
2021-12-16 01:18:25 -0600

    data loader for imitation learning

--- agil.py
+++ agil.py
@@ -1,11 +1,14 @@
 '''Example network architecture for AGIL (Attention-guided imitation learning'''
-import tensorflow as tf, numpy as np, keras as K
-import keras.layers as L
-from keras.models import Model, Sequential
+import tensorflow as tf 
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers as L
+#from tensforflow.keras.models import Model, Sequential
+from load_agil_data import some_loader
 
 BATCH_SIZE = 50
 num_epoch = 50
-num_action = 18
+num_action = 4 # act_roll,act_pitch,act_throttle,act_yaw
 SHAPE = (224,224,1) # height * width * channel 
 dropout = 0.0
 
@@ -59,20 +62,31 @@ if True:
     logits=L.Dense(num_action, name="logits")(x)
     prob=L.Activation('softmax', name="prob")(logits)
 
-    model=Model(inputs=[imgs, gaze_heatmaps], outputs=[logits, prob, g, x_intermediate])
+    model=keras.Model(inputs=[imgs, gaze_heatmaps], outputs=[logits, prob, g, x_intermediate])
     model.summary()
 
-    opt=K.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0)
-    model.compile(loss={"prob":None, "logits": K.metrics.sparse_categorical_accuracy},
-                optimizer=opt,metrics=[K.metrics.sparse_categorical_accuracy])
+    opt=keras.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0)
+    model.compile(loss={"prob":None, "logits": keras.metrics.sparse_categorical_accuracy},
+                optimizer=opt,metrics=[keras.metrics.sparse_categorical_accuracy])
 
 if __name__ == "__main__":
     # Load the Airsim Data
-    from load_data import *
-    d = Dataset(sys.argv[1], sys.argv[2]) #tarfile (images), txtfile (labels)
-    d.load_predicted_gaze_heatmap(sys.argv[3]) #npz file (predicted gaze heatmap)
-    d.standardize() 
+    import argparse
+    parser = argparse.ArgumentParser(description="AGIL Network Architecture")
+    
+    parser.add_argument("-i", "--images", type=str, help="path to images" )
+    parser.add_argument("-l", "--labels", type=str, help="path to action labels")
+    parser.add_argument("-g", "--ghmap",  type=str, help="path to predicted gaze heatmap")
+    #d = Dataset(sys.argv[1], sys.argv[2]) #tarfile (images), txtfile (labels)
+    #d.load_predicted_gaze_heatmap(sys.argv[3]) #npz file (predicted gaze heatmap)
+    #d.standardize() 
 
-    model.fit([d.train_imgs, d.train_GHmap], d.train_lbl, BATCH_SIZE, epochs=num_epoch, shuffle=True,verbose=2)
-    model.save("model.hdf5")
+    train_imgs_path = args.images
+    train_lbls_path = args.labels
+    train_gaze_path   = args.ghmap
+
+    train_imgs, train_lbls, train_gaze = some_loader(train_imgs_path, train_lbls_path, train_gaze_path)
+
+    model.fit([train_imgs, train_gaze], train_lbls, BATCH_SIZE, epochs=num_epoch, shuffle=True,verbose=2)
+    model.save("agil_model.hdf5")
 
---
+++ load_agil_data.py
@@ -0,0 +1,87 @@
+'''
+Data loader for Airsim dataset
+This file reads dataset by
+Bera et al Gaze augmented imitation learning
+'''
+
+import sys, os, re, threading, time, copy
+import numpy as np
+import csv
+import cv2
+from utils.read_gaze import reshape_heatmap, reshape_image
+
+
+def action_label(csv_path, dirname):
+    act_lbls = []
+    with open(csv_path, 'r') as csvfile:
+        act = csv.reader(csvfile)
+        next(act)  # skip the head row
+        for i, row in enumerate(act):
+            img_path = os.path.join(dirname, "rgb", f"rgb_{i}.png")
+            # print(img_path)
+            if os.path.exists(img_path):
+                print(f"rgb_{i}.png", row[-7], row[-6], row[-5], row[-4])
+
+                action_coords = np.hstack((row[-7], row[-6], row[-5], row[-4]))
+                act_lbls.append(action_coords)
+
+        act_lbls = np.array(act_lbls)
+        act_lbls = act_lbls.astype(float)
+
+    return act_lbls
+
+
+def some_loader(imgs_dir, csv_dir, gaze_dir):
+    '''
+    The function takes directory path of images, gaze and log files
+    It retruns the resized images, gaze heatmap and action label as
+    numpy array.
+    '''
+    # list all the image files and sort
+    # in order of their index
+    imgs_list =  os.listdir(imgs_dir)
+    imgs_list.sort(key=lambda f: int(re.sub('\D', '', f)))
+    gaze_list = os.listdir(gaze_dir)
+    gaze_list.sort(key=lambda g: int(re.sub('\D', '', g)))
+
+    lbls_stack = action_label(csv_dir, imgs_dir)
+    imgs_stack = []
+    gaze_stack = []
+
+    for img, gaze in zip(imgs_list, gaze_list):
+        img_path = os.path.join(imgs_dir, img)
+        gaze_path= os.path.join(gaze_dir, gaze)
+
+        image = np.float32(cv2.imread(img_path))
+        image = reshape_image(image)
+        imgs_stack.append(image)
+
+        ghmap = cv2.imread(gaze_path)
+        #ghmap = reshape_heatmap(ghmap)
+        gaze_stack.append(ghmap)
+
+    imgs_stack = np.array(imgs_stack)
+    gaze_stack = np.array(gaze_stack)
+    
+    return imgs_stack, lbls_stack, gaze_stack
+
+if __name__ == "__main__":
+
+    import argparse
+    parser = argparse.ArgumentParser(description="AGIL Network Architecture")
+
+    parser.add_argument("-i", "--images", type=str, help="path to images" )
+    parser.add_argument("-l", "--labels", type=str, help="path to action labels")
+    parser.add_argument("-g", "--ghmap",  type=str, help="path to predicted gaze heatmap")
+
+    args = parser.parse_args()
+
+    train_imgs_path = args.images
+    train_lbls_path = args.labels
+    train_gaze_path = args.ghmap
+
+    train_imgs, train_lbls, train_gaze = some_loader(train_imgs_path, train_lbls_path, train_gaze_path)
+
+    print(train_imgs.shape)
+    print(train_gaze.shape)
+    print(train_lbls.shape)
