2e21a703c8d2a3870d2172f57581dc9af0a30227
ravikt <ravikt@tamu.edu>
2022-10-30 17:40:11 -0500

    undersampling flipped data

--- utils/prepare_flipped_data.py
+++ utils/prepare_flipped_data.py
@@ -11,6 +11,7 @@ import cv2
 import numpy as np
 import os
 import re
+import pandas as pd
 
 def reshape_depth(depth):
     """Resize to the same size as the one in Ritwik's work."""
@@ -32,81 +33,79 @@ def reshape_image(image):
     return frame / 255.0
 
 
-def get_im_index(filename):
-    return re.search(r'\d+', filename).group(0)
-
 def prepare_data(data_path):
     for subdir in os.listdir(data_path):
-       print(subdir)
-       fname = ["rgb", "log.csv"]
-       dirname = os.path.join(data_path, subdir)
-       print(dirname)
+        print(subdir)
+        fname = ["rgb", "log.csv"]
+        dirname = os.path.join(data_path, subdir)
+        print(dirname)
 
-       imgs = []
-       depth= []
-       gaze_pos = []
-       act_lbls = []
+        imgs_flip = []
+        depth_flip= []
+        gaze_pos_flip = []
+        act_lbls_flip = []
         # img_idx=[]
         # print(os.path.join(dirname, fname)
-       if fname[1].endswith('.csv'):
-          log = fname[1]
-       csv_path = os.path.join(dirname, log)
-       print(csv_path)
-       with open(csv_path, 'r') as csvfile:
-            gaze = csv.reader(csvfile)
-            next(gaze)  # skip the head row
-            for i, row in enumerate(gaze):
-                img_path   = os.path.join(dirname, "rgb", f"rgb_{i}.png")
-                depth_path = os.path.join(dirname, "depth", f"depth_{i}.png")                
-                # print(img_path)
-                if os.path.exists(img_path):
-                    print(f"rgb_{i}.png", row[-3], row[-2])
-                    
-                    # read color images
-                    # flip the image horizontally 
-                    im = np.float32(cv2.flip(cv2.imread(img_path), 1))
-                    im = reshape_image(im)  
-                    imgs.append(im)
-                     
-                    # read depth images
-                    # flip the depth image horizontally
-                    dt = np.float32(cv2.flip(cv2.imread(depth_path), 1))
-                    dt = reshape_depth(dt)
-                    depth.append(dt)
+        if fname[1].endswith('.csv'):
+           log = fname[1]
+        csv_path = os.path.join(dirname, log)
+        print(csv_path)
+        train_df = pd.read_csv(csv_path)
+        train_df["rgb_addr"]  = train_df["rgb_addr"].apply(lambda x: x.split("/")[-1])
+        train_df["depth_addr"]= train_df["depth_addr"].apply(lambda x: x.split("/")[-1])
+        
+        non_zero_yaw = train_df[train_df["act_yaw"] != 0.0]
+        zero_yaw = train_df.query("act_yaw == 0.0").sample(frac=0.10)
+        final_df = pd.concat([zero_yaw, non_zero_yaw])
 
-                    # gaze coordinate
-                    # coords = np.hstack((row[-3], row[-2]))
-                    # gaze cordinates flipped
-                    coords = np.hstack((1.0-float(row[-3]), float(row[-2])))
-                    gaze_pos.append(coords)
-                    
-                    # action labels
-                    # control commands fized
-                    act_commands = np.hstack((-float(row[-7]), float(row[-6]), float(row[-5]),-float(row[-4])))
-                    act_lbls.append(act_commands)
+        for i, data in final_df.iterrows():
+            img_path   = os.path.join(dirname, "rgb", data["rgb_addr"])
+            depth_path = os.path.join(dirname, "depth", data["depth_addr"])                
+            # print(img_path)
+            # if (os.path.exists(img_path) and float(row[-4])==0.0):
+            print(data["rgb_addr"], data["gaze_x"], data["gaze_y"])
 
-            gaze_pos = np.array(gaze_pos)
-            gaze_pos = gaze_pos.astype(float)
+            # flip the image horizontally 
+            im_flip = np.float32(cv2.flip(cv2.imread(img_path), 1))
+            im_flip = reshape_image(im_flip)  
+            imgs_flip.append(im_flip)
 
-            act_lbls = np.array(act_lbls)
-            act_lbls = act_lbls.astype(float)
-            # print(gaze_pos.shape)
-            imgs = np.array(imgs)
-            depth= np.array(depth)
-            #print(imgs.shape)
+            # flip the depth image horizontally
+            dt_flip = np.float32(cv2.flip(cv2.imread(depth_path), 1))
+            dt_flip = reshape_depth(dt_flip)
+            depth_flip.append(dt_flip)
 
-      
-            print(depth.shape)
-            print(imgs.shape)
-            print(gaze_pos.shape)
-            print(act_lbls.shape)
-            #depth    = np.reshape(depth, (depth.shape[0], depth.shape[1], depth.shape[2], 1))
-            #imgs     = np.reshape(imgs, (imgs.shape[0], imgs.shape[1], imgs.shape[2], 1)) 
-            gaze_pos = np.reshape(gaze_pos, (gaze_pos.shape[0], gaze_pos.shape[1], 1))
-            act_lbls = np.reshape(act_lbls, (act_lbls.shape[0], act_lbls.shape[1], 1))
-       npz_name = data_path.split("/")[-2]
-       print(npz_name)
-       np.savez_compressed(f"flipped_{npz_name}.npz", images=imgs, depth=depth, action=act_lbls, gaze_coords=gaze_pos)
+            # gaze cordinates flipped
+            coords_flip = np.hstack((1.0-data["gaze_x"], data["gaze_y"]))
+            gaze_pos_flip.append(coords_flip)
+            
+            # control commands fized
+            act_commands_flip = np.hstack((-1.0*data["act_roll"], data["act_pitch"], data["act_throttle"], -1.0*data["act_yaw"]))
+            act_lbls_flip.append(act_commands_flip)
+
+        gaze_pos_flip = np.array(gaze_pos_flip)
+        gaze_pos_flip = gaze_pos_flip.astype(float)
+
+        act_lbls_flip = np.array(act_lbls_flip)
+        act_lbls_flip = act_lbls_flip.astype(float)
+        # print(gaze_pos.shape)
+        imgs_flip = np.array(imgs_flip)
+        depth_flip = np.array(depth_flip)
+        #print(imgs.shape)
+
+    
+        print(depth_flip.shape)
+        print(imgs_flip.shape)
+        print(gaze_pos_flip.shape)
+        print(act_lbls_flip.shape)
+
+        gaze_pos_flip = np.reshape(gaze_pos_flip, (gaze_pos_flip.shape[0], gaze_pos_flip.shape[1], 1))
+        act_lbls_flip = np.reshape(act_lbls_flip, (act_lbls_flip.shape[0], act_lbls_flip.shape[1], 1))
+
+        npz_name = data_path.split("/")[-2]
+        print(npz_name)
+        np.savez_compressed(f"flipped_{npz_name}.npz", images=imgs_flip, depth=depth_flip, action=act_lbls_flip, gaze_coords=gaze_pos_flip)
+
 
 
 if __name__ == "__main__":
@@ -118,3 +117,4 @@ if __name__ == "__main__":
     data_path = args.path
     prepare_data(data_path)
                              
+
