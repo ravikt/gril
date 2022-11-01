3a50c0e8f6ca9d89b59562892797a2013cb847bb
ravikt <ravikt@tamu.edu>
2022-11-01 15:51:35 -0500

    added random undersampling

--- utils/prepare_aril_data.py
+++ utils/prepare_aril_data.py
@@ -11,6 +11,7 @@ import cv2
 import numpy as np
 import os
 import re
+import pandas as pd
 
 def reshape_depth(depth):
     """Resize to the same size as the one in Ritwik's work."""
@@ -32,76 +33,80 @@ def reshape_image(image):
     return frame / 255.0
 
 
-def get_im_index(filename):
-    return re.search(r'\d+', filename).group(0)
-
 def prepare_data(data_path):
     for subdir in os.listdir(data_path):
-       print(subdir)
-       fname = ["rgb", "log.csv"]
-       dirname = os.path.join(data_path, subdir)
-       print(dirname)
+        print(subdir)
+        fname = ["rgb", "log.csv"]
+        dirname = os.path.join(data_path, subdir)
+        print(dirname)
 
-       imgs = []
-       depth= []
-       gaze_pos = []
-       act_lbls = []
-        # img_idx=[]
-        # print(os.path.join(dirname, fname)
-       if fname[1].endswith('.csv'):
-          log = fname[1]
-       csv_path = os.path.join(dirname, log)
-       print(csv_path)
-       with open(csv_path, 'r') as csvfile:
-            gaze = csv.reader(csvfile)
-            next(gaze)  # skip the head row
-            for i, row in enumerate(gaze):
-                img_path   = os.path.join(dirname, "rgb", f"rgb_{i}.png")
-                depth_path = os.path.join(dirname, "depth", f"depth_{i}.png")                
-                # print(img_path)
-                if os.path.exists(img_path):
-                    print(f"rgb_{i}.png", row[-3], row[-2])
-                    
-                    # read color images
-                    im = np.float32(cv2.imread(img_path))
-                    im = reshape_image(im)  
-                    imgs.append(im)
-                     
-                    # read depth images
-                    dt = np.float32(cv2.imread(depth_path))
-                    dt = reshape_depth(dt)
-                    depth.append(dt)
+        imgs = []
+        depth = []
+        gaze_pos = []
+        act_lbls = []
 
-                    # gaze coordinate
-                    coords = np.hstack((row[-3], row[-2]))
-                    gaze_pos.append(coords)
-                    
-                    # action labels
-                    act_commands = np.hstack((row[-7], row[-6], row[-5], row[-4]))
-                    act_lbls.append(act_commands)
+        if fname[1].endswith('.csv'):
+           log = fname[1]
+        csv_path = os.path.join(dirname, log)
+        print(csv_path)
+        train_df = pd.read_csv(csv_path)
+        train_df["rgb_addr"]  = train_df["rgb_addr"].apply(lambda x: x.split("/")[-1])
+        train_df["depth_addr"]= train_df["depth_addr"].apply(lambda x: x.split("/")[-1])
+        
+        non_zero_yaw = train_df[train_df["act_yaw"] != 0.0]
+        zero_yaw = train_df.query("act_yaw == 0.0").sample(frac=0.10)
+        final_df = pd.concat([zero_yaw, non_zero_yaw])
 
-            gaze_pos = np.array(gaze_pos)
-            gaze_pos = gaze_pos.astype(float)
+        for i, data in final_df.iterrows():
+            img_path   = os.path.join(dirname, "rgb", data["rgb_addr"])
+            depth_path = os.path.join(dirname, "depth", data["depth_addr"])                
+            # print(img_path)
+            # if (os.path.exists(img_path) and float(row[-4])==0.0):
+            print(data["rgb_addr"], data["gaze_x"], data["gaze_y"])
+            
+            # read color images
+            im = np.float32(cv2.imread(img_path))
+            im = reshape_image(im)  
+            imgs.append(im)
+                
+            # read depth images
+            dt = np.float32(cv2.imread(depth_path))
+            dt = reshape_depth(dt)
+            depth.append(dt)
 
-            act_lbls = np.array(act_lbls)
-            act_lbls = act_lbls.astype(float)
-            # print(gaze_pos.shape)
-            imgs = np.array(imgs)
-            depth= np.array(depth)
-            #print(imgs.shape)
+            # gaze coordinate
+            coords = np.hstack((data["gaze_x"], data["gaze_y"]))
+            gaze_pos.append(coords)
 
-      
-            print(depth.shape)
-            print(imgs.shape)
-            print(gaze_pos.shape)
-            print(act_lbls.shape)
-            #depth    = np.reshape(depth, (depth.shape[0], depth.shape[1], depth.shape[2], 1))
-            #imgs     = np.reshape(imgs, (imgs.shape[0], imgs.shape[1], imgs.shape[2], 1)) 
-            gaze_pos = np.reshape(gaze_pos, (gaze_pos.shape[0], gaze_pos.shape[1], 1))
-            act_lbls = np.reshape(act_lbls, (act_lbls.shape[0], act_lbls.shape[1], 1))
-       npz_name = data_path.split("/")[-2]
-       print(npz_name)
-       np.savez_compressed(f"{npz_name}.npz", images=imgs, depth=depth, action=act_lbls, gaze_coords=gaze_pos)
+            # control commands
+            act_commands = np.hstack((data["act_roll"], data["act_pitch"], data["act_throttle"], data["act_yaw"]))
+            act_lbls.append(act_commands)
+        
+
+        gaze_pos = np.array(gaze_pos)
+        gaze_pos = gaze_pos.astype(float)
+
+        act_lbls = np.array(act_lbls)
+        act_lbls = act_lbls.astype(float)
+
+        imgs = np.array(imgs)
+
+        depth= np.array(depth)
+
+        #print(imgs.shape)
+
+    
+        print(depth.shape)
+        print(imgs.shape)
+        print(gaze_pos.shape)
+        print(act_lbls.shape)
+        
+        gaze_pos = np.reshape(gaze_pos, (gaze_pos.shape[0], gaze_pos.shape[1], 1))
+        act_lbls = np.reshape(act_lbls, (act_lbls.shape[0], act_lbls.shape[1], 1))
+
+        npz_name = data_path.split("/")[-2]
+        print(npz_name)
+        np.savez_compressed(f"{npz_name}.npz", images=imgs, depth=depth, action=act_lbls, gaze_coords=gaze_pos)
 
 
 if __name__ == "__main__":
@@ -113,3 +118,4 @@ if __name__ == "__main__":
     data_path = args.path
     prepare_data(data_path)
                              
+
