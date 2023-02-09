1ee0bf39017617748484e4493291448be76c3968
Ravi Kumar Thakur <ravikt@users.noreply.github.com>
2023-02-08 20:57:39 -0600

    initial commit (#19)

--- utils/data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# to do - pass the path as argument 
-for value in {1..20}
-do
-  python prepare_gaze.py -p /scratch/user/ravikt/airsim/data/stationary_truck/truck_mountains$value/
-done
