60ce7b976973b5da0b9c47e1ce6fec7699777319
ravikt <ravikt@tamu.edu>
2023-02-13 11:02:01 -0600

    update

--- models.py
+++ models.py
@@ -11,11 +11,13 @@ from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPool2D
 from tensorflow.keras.layers import Concatenate
+from losses import my_softmax, my_kld, cgl_kl
 import matplotlib.pyplot as plt
 import os
 import random
 
 
+'''
 def my_softmax(x):
     """Softmax activation function. Normalize the whole metrics.
     # Arguments
@@ -26,7 +28,7 @@ def my_softmax(x):
         ValueError: In case `dim(x) == 1`.
     """
     return keras.activations.softmax(x, axis=[1, 2, 3])
-
+    
 
 def my_kld(y_true, y_pred):
     """
@@ -36,7 +38,7 @@ def my_kld(y_true, y_pred):
     y_true = keras.backend.clip(y_true, epsilon, 1)
     y_pred = keras.backend.clip(y_pred, epsilon, 1)
     return keras.backend.sum(y_true * keras.backend.log(y_true / y_pred), axis=[1, 2, 3])
-
+'''
 def agil_gaze_model():
     # Constants
     img_shape = 224 #84
@@ -104,64 +106,10 @@ def agil_gaze_model():
 
     return model
     
-
-def gaze_coord_model():
-    img_shape = 224 #84
-    k = 1
-    # Constants
-    SHAPE = (img_shape, img_shape, 1)  # height * width
-    dropout = 0.0
-    ###############################
-    # Architecture of the network #
-    ###############################
-    imgs  = L.Input(shape=SHAPE)
-    #x     = L.Reshape((224,224,1))(imgs)
-    x = imgs
-
-    conv1 = L.Conv2D(64, (5, 5), strides=2, padding='same')
-    x = conv1(x)
-    x = L.Activation('relu')(x)
-    x = L.BatchNormalization()(x)
-    x = L.Dropout(dropout)(x)
-
-    conv2 = L.Conv2D(32, (3, 3), strides=2, padding='same')
-    x = conv2(x)
-    x = L.Activation('relu')(x)
-    x = L.BatchNormalization()(x)
-    x = L.Dropout(dropout)(x)
     
-    conv3 = L.Conv2D(32, (4, 4), strides=2, padding='same')
-    x = conv3(x)
-    x = L.Activation('relu')(x)
-    x = L.BatchNormalization()(x)
-    x = L.Dropout(dropout)(x)
-
-    conv4 = L.Conv2D(64, (3, 3), strides=2, padding='same')
-    x = conv4(x)
-    x = L.Activation('relu')(x)
-    x = L.BatchNormalization()(x)
-    x = L.Dropout(dropout)(x)
-    
-    x = L.Flatten()(x)
-    x = L.Dropout(0.4)(x)
-    
-    x = L.Dense(64, activation='relu')(x)
-    x = L.Dense(32, activation='relu')(x)
-    x = L.Dense(16, activation='relu')(x)
-    #x = Dense(64, activation='tanh')(x)
-    # Output probability
-    # Sigmoid tells us the probability if input sceneflow 
-    # is real or not
-    outputs = L.Dense(2, activation='sigmoid')(x)
-    model = keras.Model(inputs=imgs, outputs=outputs)
-
-    model.summary()
-
-    return model
-
-def aril():
+def gril_mse():
    
-    resnet = tf.keras.applications.mobilenet.MobileNet(
+    mobilenet = tf.keras.applications.mobilenet.MobileNet(
     include_top=False,
     weights='imagenet',
     input_tensor=None,
@@ -169,83 +117,72 @@ def aril():
     pooling=None,
     )
 
-    resnet.trainable = False
+    mobilenet.trainable = True
     
    # RGB Channel
     rgb = Input(shape=(224,224,3), name='image')
-    # conv11 = Conv2D(32, kernel_size=4, activation='relu')(rgb)
-    # pool11 = MaxPool2D(pool_size=(2, 2))(conv11)
-    # conv12 = Conv2D(16, kernel_size=4, activation='relu')(pool11)
-    # pool12 = MaxPool2D(pool_size=(2, 2))(conv12)
-    x = resnet(rgb, training = False)
-  
-    x = Conv2D(64, (5,5), strides=2, padding='same', activation='relu')(x)
+       
+    x = mobilenet(rgb, training = True)
+    # x = L.BatchNormalization()(x)
 
-    x = Conv2D(64, (5,5), strides=2, padding='same', activation='relu')(x)
+    x = Conv2D(128, (5,5), strides=2, padding='same', activation='elu')(x)
 
-    #conv13 = Conv2D(32, (5,5), strides=2, padding='same', activation='relu')(conv12)
-
-    #conv14 = Conv2D(16, (5,5), strides=2, padding='same', activation='relu')(conv13)
-
+    x = Conv2D(64, (5,5), strides=2, padding='same', activation='elu')(x)
+    
+    x = Conv2D(64, (5,5), strides=2, padding='same', activation='elu')(x)
  
-    pool11 = MaxPool2D(pool_size=(2, 2))(x)
+    x = Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(x)
+   
+    x = Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(x)
+  
+    pool11 = MaxPool2D((2, 2), padding='same')(x)
     rgb_flat = Flatten()(pool11)
+    rgb_flat = L.Dropout(0.5)(rgb_flat)
 
     # Depth Channel
-    depth = Input(shape=(224,224,3), name='depth')
-    conv21 = Conv2D(64, (5,5), strides=2, padding='same', activation='relu')(depth)
-  
-    conv22 = Conv2D(64, (5,5), strides=2, padding='same', activation='relu')(conv21)
+    depth = Input(shape=(224,224,1), name='depth')
+    # d = L.BatchNormalization()(depth)
+    d = Conv2D(128, (5,5), strides=2, padding='same', activation='elu')(depth)
 
-    conv23 = Conv2D(32, (5,5), strides=2, padding='same', activation='relu')(conv22)
-  
-    conv24 = Conv2D(16, (5,5), strides=2, padding='same', activation='relu')(conv23)
+    d = Conv2D(64, (5,5), strides=2, padding='same', activation='elu')(d)
+
+    d = Conv2D(64, (5,5), strides=2, padding='same', activation='elu')(d)
+
+    d = Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(d)
  
-    pool21 = MaxPool2D(pool_size=(2, 2))(conv24)
+    d = Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(d)
+
+    pool21 = MaxPool2D((2, 2), padding='same')(d)
 
     depth_flat = Flatten()(pool21)
-    
-    
-    # Shared feature extraction layer
-    # merge pool12 and pool22
-    # shared_layer = Concatenate([pool12, pool22])
+    depth_flat = L.Dropout(0.5)(depth_flat)
 
     shared_layer = tf.keras.layers.concatenate([rgb_flat, depth_flat])
  
 
     # action prediction head
-    x1= Dense(512, activation='elu')(shared_layer)
-    x1= Dense(256, activation='elu')(x1)
-    x1= Dense(128, activation='elu')(x1)
-    x1= Dense(64, activation='elu')(x1)
-   
-    # action = Dense(4, activation='softmax')(x1)
+    x1 = Dense(512, activation='elu')(shared_layer)
+    x1 = L.Dropout(0.5)(x1)
+    x1 = Dense(256, activation='elu')(x1)
+    x1 = Dense(128, activation='elu')(x1)
+    x1 = Dense(64, activation='elu')(x1)
+    x1 = Dense(32, activation='elu')(x1)
     action = Dense(4, name='action')(x1)
 
 
-    # conv1 = Conv2D(32, kernel_size=3, activation='relu')(shared_layer)
-    # pool1 = MaxPool2D(pool_size=(2, 2))(conv1)
-    # action = Flatten()(pool1)
-
     # gaze prediction head
-    x2= Dense(512, activation='relu')(shared_layer)
-    x2= Dense(256, activation='relu')(x2)
-    x2= Dense(128, activation='relu')(x2)
-    x2= Dense(64, activation='relu')(x2)
-
-    # gaze= Dense(2, activation='softmax')(x2)
+    x2 = Dense(512, activation='relu')(shared_layer)
+    x2 = L.Dropout(0.5)(x2) 
+    x2 = Dense(256, activation='relu')(x2)
+    x2 = Dense(128, activation='relu')(x2)
+    x2 = Dense(64, activation='relu')(x2)
     gaze= Dense(2, name='gaze')(x2)
 
-    # conv2 = Conv2D(16, kernel_size=3, activation='relu')(shared_layer)
-    # pool2 = MaxPool2D(pool_size=(2, 2))(conv2)
-    # gaze = Flatten()(pool2)
 
     model = Model(inputs = [rgb, depth], outputs=[action, gaze])
    
     model.summary() 
     return model
-
-
 
 
 def agil_airsim(): 
@@ -293,9 +230,6 @@ def agil_airsim():
 
     orig_x=L.Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(orig_x)
 
-    #orig_x=L.Conv2D(32, (5,5), strides=2, padding='same')(orig_x)
-    #orig_x=L.BatchNormalization()(orig_x)
-    #orig_x=L.Activation('relu')(orig_x)
     orig_x= L.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(orig_x)
     orig_x= L.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(orig_x)
   
@@ -316,33 +250,153 @@ def agil_airsim():
     
     return agil_airsim_model
 
+
 def vanilla_bc():
 
-    inputs= Input(shape=(224, 224, 1))
-    x=inputs # inputs is used by the line "Model(inputs, ... )" below
-    x=Conv2D(32, (5,5), strides=2, padding='same', activation='relu')(x)
+    inputs= Input(shape=(224, 224, 3), name="image")
+    
+    x=L.Conv2D(128, (5,5), strides=2, padding='same', activation='elu')(inputs)
+    
+    x=L.Conv2D(128, (5,5), strides=2, padding='same', activation='elu')(x)
 
-    x=Conv2D(64, (3,3), strides=2, padding='same', activation='relu')(x)
-    # x=Dropout(dropout)(x)
+    x=L.Conv2D(64, (5,5), strides=2, padding='same', activation='elu')(x)
+ 
+    x=L.Conv2D(64, (5,5), strides=2, padding='same', activation='elu')(x)
 
-    x=Conv2D(64, (3,3), strides=2, padding='same', activation='relu')(x)
-    # x=L.Dropout(dropout)(x)
+    x=L.Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(x)
 
-    x=Conv2D(64, (3,3), strides=1, padding='same', activation='relu')(x)
+    x=L.Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(x)
 
-    x=Conv2D(32, (3,3), strides=1, padding='same', activation='relu')(x)
+    x= L.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+  
+ 
+    x = L.Flatten()(x)
+    x = L.Dropout(0.5)(x)
 
-    # x= Dropout(dropout)(x)
-    x= Flatten()(x)
+    x = L.Dense(256, activation='elu')(x)
+    x = L.Dropout(0.5)(x)
+    x = L.Dense(128, activation='elu')(x)
+    #x = L.Dropout(0.5)(x)
+    x = L.Dense(64, activation='elu')(x)
 
-    x= Dense(512, activation='relu')(x)
-    x= Dense(256, activation='relu')(x)
-    # output= Dense(4, activation='softmax')(x)
-    output= Dense(4)(x)
+    output= Dense(4, name="action")(x)
 
-    # x=L.Dropout(dropout)(x)
+
+    model=Model(inputs=inputs, outputs=output)
+
+    return model
+    
+
+def vanilla_bc_depth():
+
+    inputs= Input(shape=(224, 224, 1), name="image")
+    
+    x=L.Conv2D(128, (5,5), strides=2, padding='same', activation='elu')(inputs)
+    
+    x=L.Conv2D(128, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    x=L.Conv2D(64, (5,5), strides=2, padding='same', activation='elu')(x)
+ 
+    x=L.Conv2D(64, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    x=L.Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    x=L.Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    x= L.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+  
+ 
+    x = L.Flatten()(x)
+    x = L.Dropout(0.5)(x)
+
+    x = L.Dense(256, activation='elu')(x)
+    x = L.Dropout(0.5)(x)
+    x = L.Dense(128, activation='elu')(x)
+    #x = L.Dropout(0.5)(x)
+    x = L.Dense(64, activation='elu')(x)
+
+    output= Dense(4, name="action")(x)
+
 
     model=Model(inputs=inputs, outputs=output)
 
     return model
 
+def il_cgl_4():
+
+    inputs= Input(shape=(224, 224, 3), name="image")
+
+    x=L.Conv2D(128, (5,5), strides=2, padding='same', activation='elu')(inputs)
+
+    x=L.Conv2D(128, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    x=L.Conv2D(64, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    x=L.Conv2D(64, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    #x=L.Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    #x=L.Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    # CGL conv output
+    last_conv = L.Conv2D(1, (1,1), strides=1, padding='same')
+    z = last_conv(x)
+    cgl_out = L.Activation(my_softmax, name="gaze")(z)
+
+    y = L.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+
+    y = L.Flatten()(y)
+    y = L.Dropout(0.5)(y)
+
+    y = L.Dense(256, activation='elu')(y)
+    y = L.Dropout(0.5)(y)
+    y = L.Dense(128, activation='elu')(y)
+    #x = L.Dropout(0.5)(x)
+    y = L.Dense(64, activation='elu')(y)
+
+    action = Dense(4, name="action")(y)
+
+
+    model = Model(inputs=inputs, outputs=[cgl_out, action])
+
+    return model
+
+
+def il_cgl_3():
+
+    inputs= Input(shape=(224, 224, 3), name="image")
+
+    x=L.Conv2D(128, (5,5), strides=2, padding='same', activation='elu')(inputs)
+
+    x=L.Conv2D(128, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    x=L.Conv2D(64, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    # x=L.Conv2D(64, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    # x=L.Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    # x=L.Conv2D(32, (5,5), strides=2, padding='same', activation='elu')(x)
+
+    # CGL conv output
+    last_conv = L.Conv2D(1, (1,1), strides=1, padding='same')
+    z = last_conv(x)
+    cgl_out = L.Activation(my_softmax, name="gaze")(z)
+
+    y = L.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+
+    y = L.Flatten()(y)
+    y = L.Dropout(0.5)(y)
+
+    y = L.Dense(256, activation='elu')(y)
+    y = L.Dropout(0.5)(y)
+    y = L.Dense(128, activation='elu')(y)
+    #x = L.Dropout(0.5)(x)
+    y = L.Dense(64, activation='elu')(y)
+
+    action = Dense(4, name="action")(y)
+
+
+    model = Model(inputs=inputs, outputs=[cgl_out, action])
+
+    return model
